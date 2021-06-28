28f85984dc8bc4c0c59738f1f2629a3c2e26e849
AaronPacht <aharonpacht@gmail.com>
2021-06-28 18:46:22 -0400

    added player class

--- 15-assign-OOP-part1.rb
+++ 15-assign-OOP-part1.rb
@@ -13,3 +13,35 @@ class BankAccount
         @balance+=@balance/100*@interest_rate
     end
 end
+
+class Player
+    def initialize
+        @gold_coins=0
+        @health_points=10
+        @lives=5
+    end
+    def level_up
+        @lives+=1
+    end
+    def collect_treasure
+        @gold_coins+=1
+        if @gold_coins%10==0
+            level_up
+        end
+    end
+    def do_battle(dmg)
+        @health_points-=dmg
+        if @health_points<1
+            @lives-=1
+            @health_points=10
+        end
+        if @lives<1
+            restart
+        end
+    end
+    def restart
+        @lives=5
+        @health_points=10
+        @gold_coins=0
+    end
+end
